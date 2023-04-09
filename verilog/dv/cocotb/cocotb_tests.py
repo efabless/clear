ce71c6e9448cf7ec484f82f4fb0b02fd1be9dec5
M0stafaRady <mostafa.rady@efabless.com>
2023-04-09 09:40:53 -0700

    Add chain_check_sdf test for testing hold or setup violations in sdf

--- verilog/dv/cocotb/cocotb_tests.py
+++ verilog/dv/cocotb/cocotb_tests.py
@@ -2,6 +2,7 @@ from and_gate.and_gate import and_gate
 from and_gate_no_prog.and_gate_no_prog import and_gate_no_prog
 from and_gate_direct.and_gate_direct import and_gate_direct
 from chain_check.chain_check import chain_check
+from chain_check_sdf.chain_check_sdf import chain_check_sdf
 from chain_check_ones.chain_check_ones import chain_check_ones
 from and_gate_la.and_gate_la import and_gate_la
 from hello_world_uart.hello_world_uart import hello_world_uart
