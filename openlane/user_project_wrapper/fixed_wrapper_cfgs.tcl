f8bafa61b8d744306cd2f0ab33e0e15807640338
mo-hosni <m.hosni.ibra@gmail.com>
2023-04-12 08:05:07 -0700

    update `user_project_wrapper` configuration from efabless/caravel_user_project and regenerate the views

--- openlane/user_project_wrapper/fixed_wrapper_cfgs.tcl
+++ openlane/user_project_wrapper/fixed_wrapper_cfgs.tcl
@@ -26,9 +26,6 @@ set ::env(DIE_AREA) "0 0 2920 3520"
 
 set ::env(RUN_CVC) 0
 
-# Pin Configurations. DON'T TOUCH
-set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
-
 set ::unit 2.4
 set ::env(FP_IO_VEXTEND) [expr 2*$::unit]
 set ::env(FP_IO_HEXTEND) [expr 2*$::unit]
@@ -42,7 +39,7 @@ set ::env(FP_IO_HTHICKNESS_MULT) 4
 set ::env(FP_PDN_CORE_RING) 1
 set ::env(FP_PDN_CORE_RING_VWIDTH) 3.1
 set ::env(FP_PDN_CORE_RING_HWIDTH) 3.1
-set ::env(FP_PDN_CORE_RING_VOFFSET) 14
+set ::env(FP_PDN_CORE_RING_VOFFSET) 12.45
 set ::env(FP_PDN_CORE_RING_HOFFSET) $::env(FP_PDN_CORE_RING_VOFFSET)
 set ::env(FP_PDN_CORE_RING_VSPACING) 1.7
 set ::env(FP_PDN_CORE_RING_HSPACING) $::env(FP_PDN_CORE_RING_VSPACING)
@@ -53,3 +50,6 @@ set ::env(FP_PDN_HSPACING) [expr 5*$::env(FP_PDN_CORE_RING_HWIDTH)]
 set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
 set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 set ::env(SYNTH_USE_PG_PINS_DEFINES) "USE_POWER_PINS"
+
+# Pin placement template
+set ::env(FP_DEF_TEMPLATE) $::env(DESIGN_DIR)/user_project_wrapper.def
